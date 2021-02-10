e45e5d28cc26cb925c1ee180581d1cf09ed00967
Anders Leung <anders.yb.leung@gmail.com>
2021-02-09 18:52:00 -0800

    Trying something else.

--- config/express.js
+++ config/express.js
@@ -47,7 +47,7 @@ app.use(cors({ origin: config.client }));
 // }
 
 // mount all routes on /api path
-app.use('/', routes);
+app.use('/api', routes);
 
 // if error is not an instanceOf APIError, convert it.
 app.use((err, req, res, next) => {
