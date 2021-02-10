6c0c9d8166e7d4ffbf4253ee6ab18e9b607a8676
Anders Leung <anders.yb.leung@gmail.com>
2021-02-09 17:18:29 -0800

    Use new parser.

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ mongoose.Promise = Promise;
 
 // connect to mongo db
 const mongoUri = config.mongo.host;
-mongoose.connect(mongoUri, {});
+mongoose.connect(mongoUri, { useNewUrlParser: true });
 mongoose.connection.on('error', () => {
   throw new Error(`unable to connect to database: ${mongoUri}`);
 });
